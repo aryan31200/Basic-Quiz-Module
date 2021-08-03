4fdc7cfbfa8d007a90f6c9d9bad4c0c095ec2f5c
Aryan Gupta <aryan678gupta@gmail.com>
2021-08-04 00:43:42 +0530

    UI overhaul
    Made UI exactly as given in assignment

--- src/Questions.js
+++ src/Questions.js
@@ -5,77 +5,88 @@ const data = [
       Option1: "n^2 spanning trees",
       Option2: "n^(n - 2) spanning trees",
       Option3: "n^(n + 1) spanning trees",
-      Option4: "n^n spanning trees"
+      Option4: "n^n spanning trees",
+      Score: 1
     },
     {
       Question: "What is full form of CSS",
       Option1: "Cascade Style sheets",
       Option2: "Color and Style Sheets",
       Option3: "Cascaded Styling Sheets",
-      Option4: "None of these"
+      Option4: "None of these",
+      Score: 1
     },
     {
       Question: "How to include javascript in HTML document",
       Option1: "src",
       Option2: "ext",
       Option3: "script",
-      Option4: "link"
+      Option4: "link",
+      Score: 1
     },
     {
       Question: "How to declare variable in javascript",
       Option1: "var",
       Option2: "jvar",
       Option3: "obj",
-      Option4: "None of these"
+      Option4: "None of these",
+      Score: 1
     },
     {
       Question: "What => means in javascript",
       Option1: "Function literal",
       Option2: "Arrow function",
       Option3: "Invocation expression",
-      Option4: "Object Creation Expression"
+      Option4: "Object Creation Expression",
+      Score: 1
     },
     {
       Question: "Which of these is not a javascript framework",
       Option1: "JQuery",
       Option2: "Angular",
       Option3: "Django",
-      Option4: "Vue"
+      Option4: "Vue",
+      Score: 1
     },
     {
       Question: "How do you define object in js",
       Option1: "Invocation Expression",
       Option2: "[]",
       Option3: "{}",
-      Option4: "None of these"
+      Option4: "None of these",
+      Score: 1
     },
     {
       Question: "Which of these is not a valid js expression",
-      Option1: "ES5",
-      Option2: "ES6",
+      Option1: "!==",
+      Option2: "++",
       Option3: "EX19",
-      Option4: "None of these"
+      Option4: "None of these",
+      Score: 1
     },
     {
       Question: "Whats the output of '2' + 2",
       Option1: "Syntax error",
       Option2: "4",
       Option3: "22",
-      Option4: "undefined"
+      Option4: "undefined",
+      Score: 1
     },
     {
       Question: "What is the output of '2' - 1",
       Option1: "1",
       Option2: "3",
       Option3: "NaN",
-      Option4: "undefined"
+      Option4: "undefined",
+      Score: 1
     },
     {
       Question: "How to check if a value is NaN in javascript",
       Option1: "isNaN(val)",
       Option2: "isNotANumber(val)",
       Option3: "isDigit(val)",
-      Option4: "None of these"
+      Option4: "None of these",
+      Score: 1
     }
   ],
   [
--- src/index.js
+++ src/index.js
@@ -1,34 +1,94 @@
-var numOfQuestions = prompt("Enter the number of Questions 2 - 10");
-if(numOfQuestions < 2) {
-  numOfQuestions = 2;
-} 
-if(numOfQuestions > 10) {
-  numOfQuestions = 10;
-}
+var numOfQuestions = data[0].length;
 var questionSpace = document.getElementById("questions");
 var question = questionSpace.querySelector(".question-block");
-for (var i = 0; i < numOfQuestions; i++) {
-  var clone = question.cloneNode(true);
-  document.getElementsByClassName("qq")[i].innerHTML = data[0][i].Question;
-  var options = document.getElementsByClassName("qo")[i];
-  var questionNum = i;
-  for (var j = 0; j < 4; j++) {
-    options.getElementsByTagName("input")[j].name = questionNum;
-    options.getElementsByTagName("label")[j].innerText = data[0][i]["Option" + (j + 1)];
+var itr = 0, totalScore = 0;
+printQuestion();
+
+function printQuestion() {
+  if(itr > numOfQuestions - 1) {
+    answerPage();
+    return;
+  } 
+  question.querySelector(".qq span").innerText = data[0][itr].Question;
+  var options = question.querySelector(".qo");
+  for(var i = 0; i < 4; i++) {
+    options.getElementsByTagName("input")[i].name = itr + 1;
+    options.getElementsByTagName("input")[i].checked = false;
+    options.getElementsByTagName("label")[i].innerText = data[0][itr]["Option" + (i + 1)];
   }
-  clone.style.display = "block";
-  questionSpace.appendChild(clone);
+  question.style.display = "block";
+  questionSpace.appendChild(question);
 }
+
 function checkAns(event) {
+  if(event.target.parentElement.parentElement.querySelector("input:checked") === null) {
+    //console.log(totalScore);
+    alert("Please select an option");
+  }
   var question = event.target.parentElement.parentElement.querySelector("input:checked").name;
   var selectedOption = event.target.parentElement.parentElement.querySelector("input:checked").value;
-  if(data[1][question] == selectedOption) {
-    event.target.parentElement.querySelector(".response").innerText="✅Correct";
-  } else {
-    var correctAns = data[0][question][data[1][question]];
-    event.target.parentElement.querySelector(".response").innerText="❌Incorrect, the correct answer is " + correctAns;
+  
+  for(var i = 0; i < 4; i++) {
+    event.target.parentElement.parentElement.querySelectorAll("input")[i].disabled = true;
   }
+  
+  if(data[1][question - 1] == selectedOption) {
+    totalScore = totalScore + data[0][question - 1].Score;
+    event.target.parentElement.querySelector(".response").style.display = "inline-block";
+    event.target.parentElement.querySelector(".response").style.color = "#155841";
+    event.target.parentElement.querySelector(".response").style.backgroundColor= "#D4EDDA";
+    event.target.parentElement.querySelector(".response").style.border = "1px solid #28A745"
+    event.target.parentElement.querySelector(".response").innerText="Correct";
+  } else {
+    event.target.parentElement.querySelector(".response").style.display = "inline-block";
+    event.target.parentElement.querySelector(".response").style.color = "#721C24";
+    event.target.parentElement.querySelector(".response").style.backgroundColor= "#F8D7DA";
+    event.target.parentElement.querySelector(".response").style.border = "1px solid #c49da1"
+    event.target.parentElement.querySelector(".response").innerText="Incorrect";
+  }
+  var nextbtn = event.target.parentElement.querySelector("button.next");
+  event.target.style.display = "none";
+  nextbtn.style.display = "block";  
 }
+
+function nextQues(event) {
+  itr++;
+  printQuestion();
+  for(var i = 0; i < 4; i++) {
+    event.target.parentElement.parentElement.querySelectorAll("input")[i].disabled = false;
+  }
+  event.target.parentElement.querySelector(".response").style.display = "none";
+  var submitbtn = event.target.parentElement.querySelector("button.check");
+  event.target.style.display = "none";
+  submitbtn.style.display = "block";
+}
+
+function answerPage() {
+  document.getElementById("answers").style.display = "block";
+  document.getElementById("questions").style.display = "none";
+  document.getElementById("quiz-name").innerText = "Score: "+totalScore;
+  var list = document.createElement("ul");
+  for(var i = 0; i < numOfQuestions; i++) {
+    var list_item = document.createElement("li");
+    list_item.innerHTML = data[0][i].Question + " - <span>" + data[0][i][data[1][i]] + "</span>";
+    list.appendChild(list_item);
+  }
+  document.getElementById("answer-key").appendChild(list);
+}
+function restart() {
+  var answer_key = document.getElementById("answer-key");
+  while(answer_key.firstChild) {
+    answer_key.removeChild(answer_key.firstChild);
+  }
+  
+  document.getElementById("answers").style.display = "none";
+  document.getElementById("quiz-name").innerText = "Quiz";
+  totalScore = 0;
+  itr = 0;
+  document.getElementById("questions").style.display = "block";
+  printQuestion();
+}
+
 // const ans = data[1][0];
 // console.log(ans);
 // console.log((data[0][0][data[1][0]]))
